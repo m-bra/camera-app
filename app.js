0059dccc54aa683da038f8fc7b023bea6098f528
Merlin Brandt <merlin.brandt@hotmail.de>
2024-12-13 18:07:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -21,10 +21,13 @@ function cameraStart() {
         });
 }
 
+var ctxt = null;
+
 function onanimationframe() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
-    cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
+    ctxt = cameraSensor.getContext("2d");
+    ctxt.drawImage(cameraView, 0, 0);
     // let imgdata = ctxt.getImageData(0, 0, w / 2, h);
     // ctxt.putImageData(0, 0, imgdata);
     // ctxt.putImageData(w / 2, 0, imgdata);
